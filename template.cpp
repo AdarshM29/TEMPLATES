0ddf1384295ddcdbdc34fc4b8820d69e346d6846
AdarshM_29 <adarshm1020@gmail.com>
2024-08-19 17:51:12 +0530

    added input for int str vector ouput of vector

--- template.cpp
+++ template.cpp
@@ -1,10 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+#define vl vector<ll>
+#define vpl vector<pair<ll,ll>>
+#define fl(a,b) for(ll i=a;i<b;i++)
+#define ill(n) ll n;cin>>n;
+#define istr(s) string s;cin>>s;
+#define ivec(v,n) vl v(n);fl(0,n) cin>>v[i];
+#define ovec(v,n) fl(0,n){cout<<v[i]<<" ";};cout<<endl;
+#define all(x) x.begin(),x.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+
+
+void solve(){
+
+}
+
 int main(){
-    int t;
-    cin>>t;
+    ill(t);
     while(t--){
-        
+        solve();
     }
 }
